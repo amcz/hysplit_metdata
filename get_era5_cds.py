334d3af8fc18b8460689a9699007d04dfeaa3063
amcz <alicec@umd.edu>
2020-10-05 14:20:14 -0400

    get_era5_cds.py  HYSPLIT convention is that upward sensible surface heat flux is positive while ERA5 has opposite convention. Change multiplicative factor to -1.

--- get_era5_cds.py
+++ get_era5_cds.py
@@ -95,12 +95,14 @@ def getvars(means=False, tm=1, levtype='pl'):
     #    sname['SHTF'] = ['msshf','146', '1.0', '33.235'] #units W/m^2 (surface sensible heat flux)      
     #    sname['LTHF'] = ['mslhf','34', '1.0', '34.235']  #latent heat flux. same as sshf            
     #Also having trouble getting accumulated sshf to convert.
-    #sname['SHTF'] = ['sshf','146', amult,'146.128', 'surface_sensible_heat_flux'] #units J/m^2 (surface sensible heat flux) (divide by 3600 to get W/m^2)     
-    sname['LTHF'] = ['slhf','147', amult,'147.128','surface_latent_heat_flux'] #same as sshf            
+    #sname['SHTF'] = ['sshf','146', -1*amult,'146.128', 'surface_sensible_heat_flux'] #units J/m^2 (surface sensible heat flux) (divide by 3600 to get W/m^2)     
+    # HYSPLIT convention is that upward sensible heat flux should be positive. 
+    # Multiply by -1
+    sname['LHTF'] = ['slhf','147', -1*amult,'147.128','surface_latent_heat_flux'] #same as sshf            
     if instant:
         #instaneous fluxes may be more desireable since use instanteous winds.
         sname['SHTF'] =\
-                      ['ishf','231','1.0','231.128',
+                      ['ishf','231','-1.0','231.128',
                        'instantaneous_surface_sensible_heat_flux'] #instantaneous SHTF. units W/m^2.
 
     sname['DSWF'] = ['ssrd','169', amult,'169.128',
